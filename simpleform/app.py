b813597fece366f330f0272e61ee0ee5895ff320
johncoleman83 <lcsw@davidjohncoleman.com>
2017-04-25 16:48:29 -0700

    integration success on local machine for connecting input with tweet

--- simpleform/app.py
+++ simpleform/app.py
@@ -1,13 +1,26 @@
+"""
+app module
+takes user input and tweets to designated account
+"""
 import web
+import tweepy
+from credentials import *
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
 
 urls = (
     '/', 'index',
     '/confirmtweet', 'confirmtweet'
 )
-
 app = web.application(urls, globals())
-
 render = web.template.render('templates/', base="layout")
+
+def tweet_text(tweetvar):
+    """ tweets text from input variable """
+    if tweetvar != '\n':
+        api.update_status(tweetvar)
 
 class index(object):
     def GET(self):
@@ -16,6 +29,7 @@ class index(object):
     def POST(self):
         form = web.input(tweet="tweet")
         tweetvar = "%s" % (form.tweet)
+        tweet_text(tweetvar)
         return render.confirmtweet(tweetvar = tweetvar)
 
 class confirmtweet:
