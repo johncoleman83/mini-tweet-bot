bf1a4ef20c35d2348dfa6ecf0a144c026cf2572d
johncoleman83 <lcsw@davidjohncoleman.com>
2017-04-25 08:55:27 -0700

    reorganized repo into new directories, and added form feature

---
+++ twitter/mycredentials.py
@@ -0,0 +1,4 @@
+consumer_key = 'aConsumerKey-123'
+consumer_secret = 'aConsumerSecret-123'
+access_token = 'anAccessToken-123'
+access_token_secret = 'anAccessTokenSecret-123'
---
+++ twitter/retweet_follow.py
@@ -0,0 +1,26 @@
+"""
+retweet_follow module
+in for loop, finds tweet, retweets it and follows the user
+"""
+import tweepy
+from time import sleep
+from credentials import *
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+def retweet_follow:
+"""primary function that runs for loop"""
+for tweet in tweepy.Cursor(api.search, q='#opensource').items():
+    try:
+        tweet.retweet()
+        if not tweet.user.following:
+            tweet.user.follow()
+        sleep(86400)
+    except tweepy.TweepError as e:
+        print(e.reason)
+    except StopIteration:
+        break
+
+retweet_follow()
