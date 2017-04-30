26c8fbe4b6da7bbbc8c0a71941422284c2a6c3cf
johncoleman83 <lcsw@davidjohncoleman.com>
2017-04-29 22:33:48 -0700

    updated .js and mini tweet bot name throughout html

--- static/js/behavior.js
+++ static/js/behavior.js
@@ -1,41 +1,32 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-
 	var features = document.getElementById("features");
 	var featuresBox = document.getElementById("features-box");
-
 	features.addEventListener("click", function() {
-
 		if (featuresBox.className == "empty") {
 			featuresBox.className = "features-box-data";
 		} else {
 			featuresBox.className = "empty";
 		}
-
 	});
-
+});
+document.addEventListener("DOMContentLoaded", function(event) {
 	var featuresTwo = document.getElementById("features-two");
-
 	featuresTwo.addEventListener("click", function() {
-
 		if (featuresBox.className == "empty") {
 			featuresBox.className = "features-box-data";
 		} else {
 			featuresBox.className = "empty";
 		}
-
 	});
-
+});
+document.addEventListener("DOMContentLoaded", function(event) {
 	var hamburger = document.getElementById("hamburger");
 	var navigation = document.getElementById("navigation");
-
 	hamburger.addEventListener("click", function() {
-
 		if (navigation.className == "hidden") {
 			navigation.className = "visible";
 		} else {
 			navigation.className = "hidden";
 		}
-
 	});
-
 });
