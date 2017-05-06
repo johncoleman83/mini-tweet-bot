1a5cafd1c56a2dcb49b5a49a8829618328461ee1
johncoleman83 <lcsw@davidjohncoleman.com>
2017-05-05 23:53:08 -0700

    updated travis and passed pep8 tests

--- app.py
+++ app.py
@@ -8,10 +8,9 @@ import multiprocessing
 from time import sleep
 from credentials import *
 import censorship
-import sys
-import os.path
 
-#import functions
+
+# import functions
 remove_whitespace = censorship.remove_whitespace
 censor = censorship.censor
 
@@ -30,7 +29,9 @@ urls = (
 app = web.application(urls, globals())
 render = web.template.render('templates/', base="layout")
 
-#begin tweet functions here
+# begin tweet functions here
+
+
 def tweet_text(tweetvar):
     """ tweets text from input variable """
     try:
@@ -68,6 +69,7 @@ def follow_x(searchterms, xfollowers):
                 print(e.reason)
                 pass
     return retval
+
 
 def auto_retweet(searchterms, seconds, iterations):
     """ searches for tweets, attempts to retweet, follows, and repeat """
@@ -192,7 +194,7 @@ class features:
             failcount += 1
             pass
         if failcount == 5:
-            return render.confirmfeature(status="failure",error=rferror)
+            return render.confirmfeature(status="failure", error=rferror)
         else:
             return render.confirmfeature(status="success")
 
