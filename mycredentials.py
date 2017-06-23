72745fc5dd3f9ca704553af956e7ce938ee6e909
johncoleman83 <lcsw@davidjohncoleman.com>
2017-06-23 14:35:46 -0700

    added mycredentials.py back

---
+++ mycredentials.py
@@ -0,0 +1,4 @@
+consumer_key = '[YOURCOMSUMERKEY]'  # (example: '5FmNevRxQriq1')
+consumer_secret = '[YOURCOMSUMERSECRET]'  # (example: 'pMKxr4ocPV451')
+access_token = '[YOURACCESSTOKEN]'  # (example: 'ZuPzOYwz2n6o')
+access_token_secret = '[YOURACCESSTOKENSECRET]'  # (example: 'TCqaOPc8j1qP')
